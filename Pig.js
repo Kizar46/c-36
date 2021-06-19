1a0138de505e892efd0c2ddd0a8068a34caeba88
Kiser46 <kizarhussain477@gmail.com>
2021-06-19 20:14:17 +0530

    gamestate

--- Pig.js
+++ Pig.js
@@ -2,7 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
- 
+ this.gameState=true
   }
 display()
 {
@@ -11,9 +11,11 @@ if((this.body.speed)<2.5)
 {super.display();
 }else
 {
+  if(this.gameState === true){
 World.remove(world,this.body)  
-
-}}
+score+=50
+this.gameState=false
+}}}
 };
 
 
